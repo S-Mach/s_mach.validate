b76e05bd419ce7d6e62caf492fd7114fdb7ba3e4
Lance Gatlin <lance.gatlin@gmail.com>
2015-06-08 20:46:30 -0400

    work on example usage

--- src/test/scala/net/s_mach/validate/ExampleUsage.scala
+++ src/test/scala/net/s_mach/validate/ExampleUsage.scala
@@ -2,7 +2,7 @@ package net.s_mach.validate
 
 object ExampleUsage {
 
-  import s_mach.validate._
+import s_mach.validate._
 import play.api.libs.json._
 import s_mach.validate.play_json._
 
@@ -18,26 +18,26 @@ val isName = {
 
 type Age = Int
 val isAge =
-Validator.builder[Int]
- .ensure(s"must be between (0,150)") { age =>
-   0 <= age && age <= 150
- }
- .build()
+  Validator.builder[Int]
+   .ensure(s"must be between (0,150)") { age =>
+     0 <= age && age <= 150
+   }
+   .build()
 
 case class Person(id: Int, name: String, age: Int)
 
 object Person {
-implicit val validator_Person =
- Validator.builder[Person]
-   .field("id",_.id)()
-   .field("name",_.name)(
-     _.ensure(isName)
-   )
-   .field("age",_.age)(
-     _.ensure(isAge)
-   )
-   .ensure("age plus id must be less than 1000")(p => p.id + p.age < 1000)
-   .build()
+  implicit val validator_Person =
+    Validator.builder[Person]
+      .field("id",_.id)()
+      .field("name",_.name)(
+        _.ensure(isName)
+      )
+      .field("age",_.age)(
+        _.ensure(isAge)
+      )
+     .ensure("age plus id must be less than 1000")(p => p.id + p.age < 1000)
+     .build()
 }
 
 case class Family(
@@ -50,19 +50,19 @@ case class Family(
 
 object Family {
 implicit val validator_Family =
-Validator.builder[Family]
-  .field("father",_.father)()
-  .field("mother",_.mother)()
-  .field("children",_.children)()
-  .field("grandMother",_.grandMother)()
-  .field("grandFather",_.grandFather)()
-  .ensure("father must be older than children") { family =>
-    family.children.forall(_.age < family.father.age)
-  }
-  .ensure("mother must be older than children") { family =>
-    family.children.forall(_.age < family.mother.age)
-  }
-  .build()
+  Validator.builder[Family]
+    .field("father",_.father)()
+    .field("mother",_.mother)()
+    .field("children",_.children)()
+    .field("grandMother",_.grandMother)()
+    .field("grandFather",_.grandFather)()
+    .ensure("father must be older than children") { family =>
+      family.children.forall(_.age < family.father.age)
+    }
+    .ensure("mother must be older than children") { family =>
+      family.children.forall(_.age < family.mother.age)
+    }
+    .build()
 }
 
 }
