e955d7d86d504151fba2db61be5c1c2bc0a96128
Lance Gatlin <lance.gatlin@gmail.com>
2015-06-14 13:50:18 -0400

    work

--- validate-core/src/main/scala/s_mach/validate/IsValueClass.scala
+++ validate-core/src/main/scala/s_mach/validate/IsValueClass.scala
@@ -4,16 +4,21 @@ package s_mach.validate
  * A base trait for a user-defined value-class (UDVC) that is used to
  * constrain the value space of the underlying type . The UDVC attempts
  * to behave exactly as the underlying type in code. Methods
- * such as toString, hashCode and equals pass-thru to the underlying
+ * such as toString, hashCode and equals* pass-thru to the underlying
  * type. For other methods, implicit conversion from the UDVC to the
  * underling type is provided in the s_mach.validate package object.
  * Zero-runtime cost conversion to the underlying type is provided
  * automatically through Scala's value-class (see
  * http://docs.scala-lang.org/overviews/core/value-classes.html).
  *
- * Example:
- *
+ * Example value-class:
  * implicit class Name(underlying: String) extends AnyVal with IsValueType[String]
+ *
+ * Note: equals is a special case that still requires normalizing the right-hand
+ * type to match the value class. Ex:
+ *
+ * Name("Hal") == "Hal" // always returns false + compiler warning
+ * Name("Hal") == Name("Hal") // works correctly
  *
  * @tparam A type of underlying value class (Note: this parameter does not require
  *           inheritance from AnyVal since this would prevent using the trait with
@@ -23,6 +28,4 @@ trait IsValueClass[A] extends Any {
   def underlying: A
 
   override def toString = underlying.toString
-  override def hashCode = underlying.hashCode
-  override def equals(a: Any) = underlying.equals(a)
 }
