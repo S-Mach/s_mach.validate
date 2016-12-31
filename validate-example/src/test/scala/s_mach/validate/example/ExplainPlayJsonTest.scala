86e8d1db5ca198818c5d550f658379495fe2d82d
Lance Gatlin <lance.gatlin@gmail.com>
2016-12-30 21:57:33 -0500

    test coverage

--- validate-example/src/test/scala/s_mach/validate/example/ExplainPlayJsonTest.scala
+++ validate-example/src/test/scala/s_mach/validate/example/ExplainPlayJsonTest.scala
@@ -62,7 +62,7 @@ class ExplainPlayJsonTest extends FlatSpec with Matchers {
   "ExplainFormat.builder" should "allow building an ExplainFormat type-class for a product" in {
     val epj =
       ExplainPlayJson
-        .builder[Person]
+        .productBuilder[Person]
         .field("id",_.id)()
         .field("name",_.name)()
         .field("age",_.age)()
@@ -80,46 +80,45 @@ class ExplainPlayJsonTest extends FlatSpec with Matchers {
     )
   }
 
-  // todo: ordering issue
-//  "JsonValidatorPrinter.print" should "correctly print JSON for a nested case class validator" in {
-//    explainPlayJson[Family].printRemarksJson.pretty should equal(
-//"""{
-//  "this" : [ "father must be older than children", "mother must be older than children" ],
-//  "father" : {
-//    "this" : [ "age plus id must be less than 1000" ],
-//    "id" : [ "must be integer" ],
-//    "name" : [ "must be string", "must not be empty", "must not be longer than 64 characters", "must contain only letters or spaces" ],
-//    "age" : [ "must be integer", "must be greater than or equal to 0", "must be less than or equal to 150" ]
-//  },
-//  "mother" : {
-//    "this" : [ "age plus id must be less than 1000" ],
-//    "id" : [ "must be integer" ],
-//    "name" : [ "must be string", "must not be empty", "must not be longer than 64 characters", "must contain only letters or spaces" ],
-//    "age" : [ "must be integer", "must be greater than or equal to 0", "must be less than or equal to 150" ]
-//  },
-//  "children" : {
-//    "this" : [ "must be array" ],
-//    "*" : {
-//      "this" : [ "age plus id must be less than 1000" ],
-//      "id" : [ "must be integer" ],
-//      "name" : [ "must be string", "must not be empty", "must not be longer than 64 characters", "must contain only letters or spaces" ],
-//      "age" : [ "must be integer", "must be greater than or equal to 0", "must be less than or equal to 150" ]
-//    }
-//  },
-//  "grandMother" : {
-//    "this" : [ "optional", "age plus id must be less than 1000" ],
-//    "id" : [ "must be integer" ],
-//    "name" : [ "must be string", "must not be empty", "must not be longer than 64 characters", "must contain only letters or spaces" ],
-//    "age" : [ "must be integer", "must be greater than or equal to 0", "must be less than or equal to 150" ]
-//  },
-//  "grandFather" : {
-//    "this" : [ "optional", "age plus id must be less than 1000" ],
-//    "id" : [ "must be integer" ],
-//    "name" : [ "must be string", "must not be empty", "must not be longer than 64 characters", "must contain only letters or spaces" ],
-//    "age" : [ "must be integer", "must be greater than or equal to 0", "must be less than or equal to 150" ]
-//  }
-//}"""
-//    )
-//  }
+  "JsonValidatorPrinter.print" should "correctly print JSON for a nested case class validator" in {
+    explainPlayJson[Family].printRemarks.printJson[JsValue].pretty should equal(
+"""{
+  "this" : [ "father must be older than children", "mother must be older than children" ],
+  "father" : {
+    "this" : [ "age plus id must be less than 1000" ],
+    "id" : [ "must be integer" ],
+    "name" : [ "must be string", "must not be empty", "must not be longer than 64 characters", "must contain only letters or spaces" ],
+    "age" : [ "must be integer", "must be greater than or equal to 0", "must be less than or equal to 150" ]
+  },
+  "mother" : {
+    "this" : [ "age plus id must be less than 1000" ],
+    "id" : [ "must be integer" ],
+    "name" : [ "must be string", "must not be empty", "must not be longer than 64 characters", "must contain only letters or spaces" ],
+    "age" : [ "must be integer", "must be greater than or equal to 0", "must be less than or equal to 150" ]
+  },
+  "children" : {
+    "this" : [ "must be array" ],
+    "*" : {
+      "this" : [ "age plus id must be less than 1000" ],
+      "id" : [ "must be integer" ],
+      "name" : [ "must be string", "must not be empty", "must not be longer than 64 characters", "must contain only letters or spaces" ],
+      "age" : [ "must be integer", "must be greater than or equal to 0", "must be less than or equal to 150" ]
+    }
+  },
+  "grandMother" : {
+    "this" : [ "optional", "age plus id must be less than 1000" ],
+    "id" : [ "must be integer" ],
+    "name" : [ "must be string", "must not be empty", "must not be longer than 64 characters", "must contain only letters or spaces" ],
+    "age" : [ "must be integer", "must be greater than or equal to 0", "must be less than or equal to 150" ]
+  },
+  "grandFather" : {
+    "this" : [ "optional", "age plus id must be less than 1000" ],
+    "id" : [ "must be integer" ],
+    "name" : [ "must be string", "must not be empty", "must not be longer than 64 characters", "must contain only letters or spaces" ],
+    "age" : [ "must be integer", "must be greater than or equal to 0", "must be less than or equal to 150" ]
+  }
+}"""
+    )
+  }
 
 }
